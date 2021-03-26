c9d776ed3fac5e70757015e442a186aca8624027
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-26 18:52:13 -0300

    Entrega de la actividad 7, a pedido del docente.

---
+++ Actividad_07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("\r");
+
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
+
+        }
+    }
+}
